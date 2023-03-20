986a85e810de60def409fc6d2c2baaacbb81d015
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-03-19 22:17:04 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+import { setSearchFocus } from "./searchBar.js";
+import { getSearchTerm } from "./dataFunctions.js";
+import { retrieveSearchResults} from "./dataFunctions.js";
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+});
+
+const initApp = () => {
+    // set the focus
+    setSearchFocus();
+    // TODO: 3 listeners clear text
+
+    const form = document.getElementById("searchBar");
+    form.addEventListener("submit", submitTheSearch);
+};
+
+// Procedural "workflow" function
+const submitTheSearch = (event) => {
+    event.preventDefault();
+    // TODO: delete search results
+    // process the search
+    processTheSearch();
+    // set the focus
+    setSearchFocus();
+};
+
+// Procedure
+const processTheSearch = async () => {
+    // TODO: clear the stats line
+    const searchTerm = getSearchTerm();
+    if (searchTerm === "") return;
+    const resultArray = await retrieveSearchResults(searchTerm);
+    if (resultArray.length) // TODO: build search results
+    // TODO: set stats line
+};
